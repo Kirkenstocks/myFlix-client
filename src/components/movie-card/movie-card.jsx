a8cf293eb07b27ed35e15f67fbb0bd45c2d57b58
Kirk Meyer <kirkmeyer03@gmail.com>
2023-11-14 19:46:43 -0800

    Imported and implemented prop-types into MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div onClick={() => {
@@ -6,4 +8,10 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired}).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
