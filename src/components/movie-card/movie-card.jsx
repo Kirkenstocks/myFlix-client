9b167e1a6b293aea2996ed40ae5a2376c7578891
Kirk Meyer <kirkmeyer03@gmail.com>
2023-11-21 19:51:23 -0800

    Updated to be displayed as a bootstrap card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,15 +1,23 @@
 import PropTypes from "prop-types";
+import Card from "react-bootstrap/Card";
+import "./movie-card.scss";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div onClick={() => {
-      onMovieClick(movie);
-    }}>
-      {movie.title}
-    </div>
+    <Card 
+      onClick={() => onMovieClick(movie)}
+      className="movie-card h-100"
+    >
+      <Card.Img variant="top" src={movie.imagePath} />
+      <Card.Body>
+        <Card.Title className="card-title">{movie.title}</Card.Title>
+        <Card.Text>{movie.description}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
+//defining prop-type constraints
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired}).isRequired,
