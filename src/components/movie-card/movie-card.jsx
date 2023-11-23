186f87c5917d615db46693744704ef47d2a6d4bc
Kirk Meyer <kirkmeyer03@gmail.com>
2023-11-22 19:03:10 -0800

    Refactored movie-card to be accessed by routing by movie id

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,20 @@
+import React from "react";
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
-    <Card 
-      onClick={() => onMovieClick(movie)}
-      className="movie-card h-100"
-    >
+    <Card className="movie-card h-100">
       <Card.Img variant="top" src={movie.imagePath} />
       <Card.Body>
         <Card.Title className="card-title">{movie.title}</Card.Title>
         <Card.Text>{movie.description}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
