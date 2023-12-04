b2975f33a60f427f8b067d4d96e88ef14e641509
Kirk Meyer <kirkmeyer03@gmail.com>
2023-12-03 20:21:13 -0800

    Initialized profile-view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -7,3 +7,10 @@ import { MovieCard } from "../movie-card/movie-card";
 import { DeleteAccountModal } from "../delete-account-modal/delete-account-modal";
 import "./profile-view.scss";
 
+export const ProfileView = ( { user, token, setUser, movies, onLoggedOut, handleShow, handleClose } ) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const storedToken = localStorage.getItem("token");
+
