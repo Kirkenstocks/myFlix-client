234176b8de077508011991acf81c13f9b2a06e4c
Kirk Meyer <kirkmeyer03@gmail.com>
2023-12-03 20:18:17 -0800

    Created function to add movie to favorites list

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,7 +5,42 @@ import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, user, setUser }) => {
+  const storedToken = localStorage.getItem("token");
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    if (user.FavoriteMovies.includes(movie.id)) {
+      setIsFavorite(true);
+    }
+  }, [user]);
+  
+  //allow users to add a movie to their favorites list
+  const addFavorite = () => {
+    fetch(`https://myflixapi-3voc.onrender.com/users/${user.Username}/movies/${movie.id}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${storedToken}`
+      }
+    }).then((response) => {
+      if(response.ok) {
+        return response.json();
+      } else {
+        alert("Unable to add movie to favorites")
+      }
+    }).then((user) => {
+      if(user) {
+        alert("Movie added to favorites");
+        localStorage.setItem("user", JSON.stringify(user));
+        setUser(user);
+        setIsFavorite(true);
+      }
+    })
+    .catch((error) => {
+      alert(error);
+    });
+  };
+
   return (
     <Card className="movie-card h-100">
       <Card.Img variant="top" src={movie.imagePath} />
