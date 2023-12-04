ca4c2fa1c80950f40155cc876ad685348dd8f1ad
Kirk Meyer <kirkmeyer03@gmail.com>
2023-12-03 20:20:44 -0800

    Imported needed files

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { MovieCard } from "../movie-card/movie-card";
+import { DeleteAccountModal } from "../delete-account-modal/delete-account-modal";
+import "./profile-view.scss";
+
