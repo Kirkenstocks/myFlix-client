e45224b9e0efa14e7938273b9e6a2892b3f74d62
Kirk Meyer <kirkmeyer03@gmail.com>
2023-12-09 19:02:44 -0800

    Updated layout

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
@@ -11,37 +12,39 @@ export const MovieView = ({ movies }) => {
 
   return (
     <Row className="justify-content-center">
-      <div>
+      <Col className="mt-3" lg={6} md={8} xs={9}>
         <img 
           src={movie.imagePath} 
           alt="poster of the movie selected" 
           className="w-100 mb-3" 
         />
-      </div>
+      </Col>
+      <Col className="mt-lg-3 mt-md-1" lg={6} md={8} xs={9}>
         <h4 className="movie-title mb-3">{movie.title}</h4>
-      <div>
-        <span>Release Year: </span>
-        <span>{movie.releaseYear}</span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.description}</span>
-      </div>
-      <div>
-        <span>Genre: </span>
-        <span>{movie.genre.Name}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.director.Name}</span>
-      </div>
-      <div>
-        <span>Featured: </span>
-        <span>{movie.featured.toString()}</span>
-      </div>
-      <Link to={`/`}>
-        <Button className="back-button mt-3 w-25">Back</Button>
+        <div>
+          <span>Release Year: </span>
+          <span>{movie.releaseYear}</span>
+        </div>
+        <div>
+          <span>Description: </span>
+          <span>{movie.description}</span>
+        </div>
+        <div>
+          <span>Genre: </span>
+          <span>{movie.genre.Name}</span>
+        </div>
+        <div>
+          <span>Director: </span>
+          <span>{movie.director.Name}</span>
+        </div>
+        <div>
+          <span>Featured: </span>
+          <span>{movie.featured.toString()}</span>
+        </div>
+        <Link to={`/`}>
+          <Button className="back-button mt-3 w-25">Back</Button>
       </Link>
+      </Col>
     </Row>
   );
 };
