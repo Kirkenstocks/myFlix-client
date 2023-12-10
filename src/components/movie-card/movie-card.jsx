b65f3f511aadc4ab5331965096cf8d5097a0e190
Kirk Meyer <kirkmeyer03@gmail.com>
2023-12-09 19:02:21 -0800

    Added heart buttons and removed movie description

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -30,7 +30,6 @@ export const MovieCard = ({ movie, user, setUser }) => {
       }
     }).then((user) => {
       if(user) {
-        alert("Movie added to favorites");
         localStorage.setItem("user", JSON.stringify(user));
         setUser(user);
         setIsFavorite(true);
@@ -55,7 +54,6 @@ export const MovieCard = ({ movie, user, setUser }) => {
       }
     }).then((user) => {
       if(user) {
-        alert("Movie removed from favorites");
         localStorage.setItem("user", JSON.stringify(user));
         setUser(user);
         setIsFavorite(false);
@@ -68,21 +66,50 @@ export const MovieCard = ({ movie, user, setUser }) => {
 
   return (
     <Card className="movie-card h-100">
-      <Card.Img variant="top" src={movie.imagePath} />
+      <Card.Img variant="top" src={movie.imagePath} className="h-100" />
       <Card.Body>
         <Card.Title className="card-title text-center">{movie.title}</Card.Title>
-        <Card.Text>{movie.description}</Card.Text>
         <div className="d-flex justify-content-between">
           <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
             <Button variant="primary" className="open-button">Open</Button>
           </Link>
           {!isFavorite ? (
-            <Button variant="link" className="favorite-button" onClick={addFavorite}>Favorite</Button>
+            <Button
+              variant="link"
+              className="favorite-button"
+              onClick={addFavorite}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="35"
+                height="35"
+                fill="red"
+                viewBox="0 0 16 16"
+              >
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+              </svg>
+            </Button>
           ) : (
-            <Button variant="link" className="favorite-button" onClick={removeFavorite}>Unfavorite</Button>
+            <Button
+              variant="link"
+              className="favorite-button"
+              onClick={removeFavorite}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="35"
+                height="35"
+                fill="red"
+                viewBox="0 0 16 16"
+              >
+                <path
+                    fillRule="evenodd"
+                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                />
+              </svg>
+            </Button>
           )}
         </div>
-        
       </Card.Body>
     </Card>
   );
